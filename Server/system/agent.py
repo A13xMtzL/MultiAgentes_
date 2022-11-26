a9cc49df08f309ddb747159b024f0bd034910d11
Alejandro Martínez Luna <A01276785@tec.mx>
2022-11-25 19:38:08 -0600

    Changes on model and agent script

--- Server/system/agent.py
+++ Server/system/agent.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import time
 import datetime
+import random
 
 from mesa import Agent, Model
 from mesa.space import SingleGrid
@@ -9,61 +10,82 @@ from mesa.time import BaseScheduler
 from mesa.datacollection import DataCollector
 
 
+LIMIT_RANGE = 20
+
+
 class CarSimulation(Agent):
-    def __init__(self, unique_id, model, pos_x, pos_y, length=0, orientation=0):
+    def __init__(self, unique_id, model, pos_x, pos_y, length=0, orientation=0, choosen=False):
         super().__init__(unique_id, model)
 
         '''Posiciones en x,y.
           Velocidad Inicial'''
-        self.unique_id = unique_id
         self.pos_x = pos_x
         self.pos_y = pos_y
-        # self.speed = 60
-        self.isActive = True
+        self.speed = 1
+        self.Activated = True
         self.length = length
         self.orientation = orientation
-
-    # Make a function named 'check_neighbors' that checks if there is a car in 5 cells in front of the car in the same row
+        self.choosen = choosen
 
     # Hace una comprobación de la misma fila para ver si hay alguien adelante
     def check_neighbors(self):
-        # Check if there is a car in the next cell
+        # Verifica si hay un vehículo en la siguiente celda
         for neighbor in self.model.grid.get_neighbors(self.pos, moore=True, include_center=False):
             if neighbor.pos_x == self.pos_x and neighbor.pos_y == (self.pos_y + 1):
                 return False
-        # Check if there is a car in the next 4 cells
-            for i in range(4):
+            # Verifica si hay un vehículo en las siguientes tres celdas
+            for i in range(3):
                 if neighbor.pos_x == self.pos_x and neighbor.pos_y == (self.pos_y + i):
                     return False
+
         return True
 
-    # Create a function that checks if the top row has the next five cells available using the check_neighbors function, if available the agent moves to this new row
-    def change_lane(self):
-        movement = self.check_neighbors()
-
-        if movement == True:
-            if self.pos_x == 0:
-                self.pos_x = 1
-                self.model.grid.move_agent(self, (self.pos_x, self.pos_y))
-            elif self.pos_x == 1:
-                self.pos_x = 0
-                self.model.grid.move_agent(self, (self.pos_x, self.pos_y))
-
+    # Función que mueve el agente hacia adelante
     def move_forward(self):
         movement = self.check_neighbors()
-
         if movement == True:
-            self.pos_y = self.pos_y + 1
+            self.pos_y = self.pos_y + self.speed
             self.model.grid.move_agent(self, (self.pos_x, self.pos_y))
-
         elif movement == False:
             self.change_lane()
 
+    # Function that checks if the cell above or below is available, if so, moves the agent to one of them
+    def change_lane(self):
+        # Check if the cell above is available
+        for neighbor in self.model.grid.get_neighbors(self.pos, moore=True, include_center=False):
+            if neighbor.pos_x == (self.pos_x - 1) and neighbor.pos_y == self.pos_y:
+                return False
+
+        # Check if the cell below is available
+        for neighbor in self.model.grid.get_neighbors(self.pos, moore=True, include_center=False):
+            if neighbor.pos_x == (self.pos_x + 1) and neighbor.pos_y == self.pos_y:
+                return False
+
+        # If both cells are available, randomly choose one of them
+        random_lane = random.randint(0, 1)
+
+        if random_lane == 0:
+            self.pos_x = self.pos_x - 1
+            self.model.grid.move_agent(self, (self.pos_x, self.pos_y))
+        else:
+            self.pos_x = self.pos_x + 1
+            self.model.grid.move_agent(self, (self.pos_x, self.pos_y))
+
+    # Function that takes just one random car that is in the middle row and makes it to stop just in the middle of the grid
+
+    def stop_random_car(self):
+        # for agent in self.model.schedule.agents:
+        if self.choosen == True and self.pos_y >= LIMIT_RANGE:
+            self.model.grid.move_agent(self, (self.pos_x, self.pos_y))
+
     def step(self):
-        self.move_forward()
+        if self.choosen == True and self.pos_y >= LIMIT_RANGE:
+            self.stop_random_car()
+        else:
+            self.move_forward()
 
     def __str__(self):
-        return f"Id del vehiculo: {self.unique_id}, Position: {self.position}"
+        return f"Id del vehiculo: {self.unique_id}, Posicion en x : {self.pos_x}, Posicion en y : {self.pos_y}, Velocidad: {self.speed}, Elegido: {self.choosen}"
 
     def json(self):
-        return {"unique_id": self.unique_id, "position": {"Posicion en x: ": int(self.pos_x), "Posicion en y: ": int(self.pos_y)}}
+        return {"unique_id": self.unique_id, "position": {"Posicion en x: ": int(self.pos_x), "speed": int(self.speed), "choosen": self.choosen, "Posicion en y: ": int(self.pos_y)}}
--- Server/system/model.py
+++ Server/system/model.py
@@ -6,23 +6,32 @@ from mesa.time import BaseScheduler
 
 from mesa.datacollection import DataCollector
 import numpy as np
-import pandas as pd
+import random
 
 from system.agent import CarSimulation
 
+LIMIT_RANGE = 20
+
 
 class HighWay (Model):
-    def __init__(self, width, height, initial_population=4):
-        self.step_count = 0
-        self.width = width
-        self.height = height
+    def __init__(self, width=3, height=100, initial_population=150, choosen_agent=False):
         self.schedule = BaseScheduler(self)
         self.grid = SingleGrid(width, height, False)
         self.initial_population = initial_population
-        self.num_agents = 100
         self.id_increment = 0
+        self.choosen_agent = choosen_agent
+        self.num_steps = 0
+        self.chose = 0
+        self.width = width
+        self.height = height
 
+        # self.datacollector = DataCollector(
+        #     model_reporters={"Grid": get_grid}
+        # )
+
+        #pos_x = np.random.choice([0, 1, 2])
         pos_x = np.random.choice([0, 1, 2])
+
         pos_y = 0
         car = CarSimulation(0, self, pos_x, pos_y)
 
@@ -32,29 +41,53 @@ class HighWay (Model):
         self.id_increment += 1
 
     def step(self):
-        self.step_count += 1
+        # self.datacollector.collect(self)
         self.schedule.step()
 
-        if self.id_increment < self.num_agents and self.id_increment >= 1:
-            pos_x = np.random.choice([0, 1, 2])
-            pos_y = 0
+        if self.id_increment < self.initial_population and self.id_increment >= 1:
+            prob = random.randint(0, 3)
+            if prob == 1 or prob == 3 or prob == 2:
+                if self.num_steps == 10 and self.chose == 0:
+                    # if self.num_steps == 10:
+                    pos_x = 1
+                    pos_y = 0
 
-            car = CarSimulation(self.id_increment, self, pos_x, pos_y)
+                    car = CarSimulation(self.id_increment, self,
+                                        pos_x, pos_y, choosen=True)
 
-            self.grid.place_agent(car, (pos_x, pos_y))
+                    self.grid.place_agent(car, (pos_x, pos_y))
 
-            self.schedule.add(car)
+                    self.schedule.add(car)
 
-            self.id_increment += 1
+                    self.id_increment += 1
+                    self.num_steps += 1
+                    self.chose = 1
+
+                else:
+                    self.chose = 0
+                    pos_x = np.random.choice([0, 1, 2])
+                    pos_y = 0
+
+                    if pos_x == 1 and pos_y < LIMIT_RANGE - 1:
+                        car = CarSimulation(
+                            self.id_increment, self, pos_x, pos_y)
+
+                        self.grid.place_agent(car, (pos_x, pos_y))
+
+                        self.schedule.add(car)
+
+                        self.id_increment += 1
+
+                        self.num_steps += 1
 
     def __str__(self):
-        return f"Width: {self.width},Height: {self.height}, Numero de carros iniciales: {self.initial_population}"
+        return f"Width: {self.width},Height: {self.height}, Numero de carros iniciales: {self.initial_population}, Numero de steps: {self.num_steps}, Numero de carros: {self.id_increment},Carro elegido: {self.choosen_agent}"
 
     def json(self):
 
-        # positions_list = []
-        # for idx in range(0, len(self.schedule.num_agents)):
-        #     p = self.schedule.num_agents[idx].json()
-        #     positions_list.append(p)
-        # positions = positions_list
-        return {"width": self.width, "height": self.height, "initial_population": self.initial_population}
+        PositionList = []
+        for idx in range(0, len(self.schedule.agents)):
+            p = self.schedule.agents[idx].json()
+            PositionList.append(p)
+        positions = PositionList
+        return {"width": self.width, "height": self.height, "initial_population": self.initial_population, "num_steps": self.num_steps, "Numero de Agentes": (self.id_increment+1), "positions": positions}
